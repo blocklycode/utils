8435c1e63508accf2408d5c228e28c8b43112604
lifeisfakenews <lifeisfakenews69@gmail.com>
2025-04-17 12:53:20 +0100

    Componenent stuff

--- auth/client.ts
+++ auth/client.ts
@@ -1,4 +1,7 @@
 import { createAuthClient } from "better-auth/react"
-export const client = createAuthClient({
-    baseURL: "http://localhost:3000" // the base url of your auth server
-})
+
+export const client = (baseUrl:string) => {
+    return createAuthClient({
+        baseURL: baseUrl, // the base url of your auth server
+    })
+};
--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,16 @@
+import Account from './Account.astro';
+import CommentSection from './CommentSection.astro';
+import Confirm from './Confirm.astro';
+import Dropdown from './Dropdown.astro';
+import DropdownItem from './DropdownItem.astro';
+import ExternalLink from './ExternalLink.astro';
+import Filters from './Filters.astro';
+import Footer from './Footer.astro';
+import Head from './Head.astro';
+import Header from './Header.astro';
+import Input from './Input.astro';
+import Modal from './Modal.astro';
+import PageError from './PageError.astro';
 import User from './User.astro';
 
-
-export { User };
+export { Account, CommentSection, Confirm, Dropdown, DropdownItem, ExternalLink, Filters, Footer, Head, Header, Input, Modal, PageError, User };
