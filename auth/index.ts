eabf32418a90a5f65311bcaa331c72fdbd49d851
lifeisfakenews <lifeisfakenews69@gmail.com>
2025-04-17 12:20:46 +0100

    I forgot to add files again

---
+++ auth/index.ts
@@ -0,0 +1,6 @@
+import { auth } from "./auth";
+import { handler } from "./handler";
+import { middleware } from "./middleware";
+import { client } from "./client";
+
+export { auth, handler, middleware, client };
